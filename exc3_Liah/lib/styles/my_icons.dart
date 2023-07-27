447f19dfb2d54e5476c52d42fb1bb4034a80a8d5
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2023-07-27 18:39:14 +0300

    <remove exs3 app>

--- exc3_Liah/lib/styles/my_icons.dart
+++
@@ -1,6 +0,0 @@
-const iconClose = 'assets/icons/close.svg';
-const iconCamera = 'assets/icons/camera.svg';
-const iconEvent = 'assets/icons/event.svg';
-const iconFacebook = 'assets/icons/facebook.svg';
-const iconInstegram = 'assets/icons/instagram.svg';
-const iconWhatsapp = 'assets/icons/whatsapp.svg';
