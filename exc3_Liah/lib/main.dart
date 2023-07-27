4bcdc808b6a19643133aed6f8f8a2026e608253b
LiaTauber9 <100442290+LiaTauber9@users.noreply.github.com>
2023-07-27 17:50:47 +0300

    <Added my excercise for code review>

---
+++ exc3_Liah/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './screens/pop_up_share.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'Assistant',
+        canvasColor: Colors.white,
+        useMaterial3: true,
+      ),
+      home: const Directionality(
+          textDirection: TextDirection.rtl, child: PopUpShare()),
+    );
+  }
+}
